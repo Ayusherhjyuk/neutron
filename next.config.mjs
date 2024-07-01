c37d6e5e6b033ddedb91931f2ecf0369defee5ec
Ayush Jadhao <ayushjadhao8804@gmail.com>
2024-07-01 11:50:25 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
+// Define your Next.js configuration
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
- 
-        
-        
-    images: {
-        unoptimized: true,
-      },
-      
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    // Increase the timeout limit as per your requirement
+    staticPageGenerationTimeout: 3000,
+  },
 };
 
-export default nextConfig;
+
+
+export default nextConfig
+
+
